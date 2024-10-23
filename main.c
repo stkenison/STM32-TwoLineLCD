7b9c113b42a0e66f82b32f8373f76d8af5b65228
Spencer Kenison <158218891+stkenison@users.noreply.github.com>
2024-10-23 13:14:19 -0600

    Modified main.c to receive typing from 4x4 keypad

--- main.c
+++ main.c
@@ -3,12 +3,63 @@
 
 int main(void){
 	
+	
 	LCD_Init(); //Initialize LCD
 	LCD_Clear(); //Clear LCD
-	LCD_DisplayString(0, (unsigned char*)"ECE 3710"); //Write ECE 3710 to LCD
-	LCD_DisplayString(1, (unsigned char*)"Utah State"); //Write Utah State to LCD
+	LCD_DisplayString(0, (unsigned char*)"Avoid pressing"); //Write ECE 3710 to LCD
+	LCD_DisplayString(1, (unsigned char*)"2 buttons"); //Write Utah State to LCD
+	delay_ms(2000);
+	LCD_Clear();
+	Keypad_Init();
 	
-	while(1);
+	unsigned char key;
+	char str[50];
+	unsigned char len = 0;
+	unsigned char max_len = 33;
+	
+	
+	
+	while(1){
+		key = Keypad_Scan(); //Set key to key pressed
+		
+		switch (key){
+			
+			//if * is pressed, clear screen
+			case '*':
+				LCD_Clear(); //clear LCD
+				str[0] = '\0';
+				str[16] = '\0';
+				len = 0;
+				break;
+			
+			//if # is pressed, switch lines
+			case '#':
+				str[len] = ' ';
+				str[len+1] = 0; //Null string terminator
+				len++;
+				if (len >= max_len) {
+					len = 0;
+				}
+				break;
+			
+			//if no key is pressed, do nothing
+			case 0xFF:
+				break;
+			
+			//add key pressed to string
+			default:
+				str[len] = key;
+				str[len+1] = 0; //Null string terminator
+				len++;
+				if (len >= max_len) {
+					len = 0;
+				}
+		}
+		
+		if (len<17) LCD_DisplayString(0, (uint8_t *)str);
+		if (len>=17) LCD_DisplayString(1, (uint8_t *)(str + 16));  // Shift the pointer to the 17th character
+		
+	}
 }
 
 
